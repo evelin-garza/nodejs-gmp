9b48405180e5ea0e42102eaa49cdbd07ef18fa3a
Evelin Garza <evelin.garza@astrazeneca.com>
2021-03-20 16:33:42 -0600

    Adding code for task 1.2

---
+++ task1.2/index.js
@@ -0,0 +1,35 @@
+const csv = require('csvtojson');
+const fs = require('fs')
+
+const csvFilePath = './csv/nodejs19-hw1-ex1.csv'
+const txtFilePath = './task1.2/nodejs19-hw1-ex2.txt';
+
+const convertCsvToTxtFile = async () => {
+  try {
+    const jsonArray = await csv().fromFile(csvFilePath);
+    const txtFileContent = jsonArrayToString(jsonArray, '\n');
+    writeFile(txtFileContent, txtFilePath);
+  } catch (err) {
+    console.log(`Reading csv file error: ${err}`);
+  }
+};
+
+const jsonArrayToString = (jsonArray, delimiter = '') => {
+  let string = '';
+  jsonArray.forEach((obj) => {
+    string += `${JSON.stringify(obj)}${delimiter}`;
+  });
+  return string;
+};
+
+const writeFile = (fileContent, filePath) => {
+  fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+      console.log(`Writing file error: ${err}`);
+    } else {
+      console.log('File written successfully!');
+    }
+  });
+};
+
+convertCsvToTxtFile();
