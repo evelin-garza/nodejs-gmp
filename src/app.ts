7e8749e03aef100d225102a64fb0f9e80d1c27d9
Evelin Garza <evelin.garza@astrazeneca.com>
2021-06-09 11:56:04 -0600

    Adding cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { UserRoutes } from './routes/user-routes';
 import { GroupRoutes } from './routes/group-routes';
 import { executionTime } from './middlewares/execution-time-middleware';
 import { AuthRoutes } from './routes/auth-route';
+import cors from 'cors';
 
 const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(executionTime);
+app.use(cors());
 
 // Routes
 app.use('/api/users', UserRoutes);
